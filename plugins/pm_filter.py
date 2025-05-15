33f1353cac732a5cb0657484b8833fd5abfb765f
ansil988 <ansil6782@gmail.com>
2025-05-15 17:40:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1435,7 +1435,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="https://t.me/KL10Admin")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="https://t.me/New_MoviesFile")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
