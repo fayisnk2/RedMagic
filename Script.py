39b56937e6e4bd0998d5b07fb794e5deaeacbf2e
ansil988 <ansil6782@gmail.com>
2025-09-07 10:10:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -14,7 +14,7 @@ Mʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>,
 Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
 
     ABOUT_TXT = """<b>✯ Mʏ Nᴀᴍᴇ: {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/New_MoviesFile'>ᴍᴀꜱᴛᴇʀ</a>"""
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/KL10Admin'>ᴍᴀꜱᴛᴇʀ</a>"""
 
     SOURCE_TXT = """<b>ɴᴏᴛᴇ:
 - ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ᴀɴ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ."""
@@ -160,8 +160,8 @@ Nᴀᴍᴇ - {}"""
 <b>⍟───[ ᴏᴡɴᴇʀ ᴅᴇᴛᴀɪʟꜱ ]───⍟
     
 • ꜰᴜʟʟ ɴᴀᴍᴇ :  ᴍᴀꜱᴛᴇʀ
-• ᴜꜱᴇʀɴᴀᴍᴇ : @New_MoviesFile
-• ᴘᴇʀᴍᴀɴᴇɴᴛ ᴅᴍ ʟɪɴᴋ : <a href='https://t.me/New_MoviesFile'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>"""
+• ᴜꜱᴇʀɴᴀᴍᴇ : @KL10Admin
+• ᴘᴇʀᴍᴀɴᴇɴᴛ ᴅᴍ ʟɪɴᴋ : <a href='https://t.me/KL10Admin'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>"""
 
     REQINFO = """
 ⚠ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ⚠
